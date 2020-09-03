8a2eb1e212688a70e099d74dd9fa4a4932a9a06f
22earth <zhifengle@gmail.com>
2020-09-03 16:56:29 +0800

    update(android): words

---
+++ demos/test-android/Words/app/src/main/java/com/example/words/Word.java
@@ -0,0 +1,54 @@
+package com.example.words;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Word {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "english_word")
+    private String word;
+    @ColumnInfo(name = "chinese_meaning")
+    private String chineseMeaning;
+    @ColumnInfo(name = "chinese_invisible")
+    private boolean chineseInvisible;
+
+    public Word(String word, String chineseMeaning) {
+        this.word = word;
+        this.chineseMeaning = chineseMeaning;
+    }
+
+    public boolean isChineseInvisible() {
+        return chineseInvisible;
+    }
+
+    public void setChineseInvisible(boolean chineseInvisible) {
+        this.chineseInvisible = chineseInvisible;
+    }
+
+    public String getChineseMeaning() {
+        return chineseMeaning;
+    }
+
+    public void setChineseMeaning(String chineseMeaning) {
+        this.chineseMeaning = chineseMeaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
