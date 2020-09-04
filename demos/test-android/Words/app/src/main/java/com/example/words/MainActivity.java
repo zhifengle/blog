d12125d825cacbe5982502b4869b4246c3a7ac5c
22earth <zhifengle@gmail.com>
2020-09-04 16:57:59 +0800

    update(android): words fragment

--- demos/test-android/Words/app/src/main/java/com/example/words/MainActivity.java
+++ demos/test-android/Words/app/src/main/java/com/example/words/MainActivity.java
@@ -14,103 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonInsert,buttonClear;
-    WordViewModel wordViewModel;
-    RecyclerView recyclerView;
-    Switch aSwitch;
-    MyAdapter myAdapter1,myAdapter2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.recyclerView);
-
-        //wordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
-        wordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
-
-        myAdapter1 = new MyAdapter(false,wordViewModel);
-        myAdapter2 = new MyAdapter(true,wordViewModel);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(myAdapter1);
-        aSwitch = findViewById(R.id.switch1);
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    recyclerView.setAdapter(myAdapter2);
-                } else {
-                    recyclerView.setAdapter(myAdapter1);
-                }
-            }
-        });
-
-
-
-        wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
-            @Override
-            public void onChanged(List<Word> words) {
-                int temp = myAdapter1.getItemCount();
-                myAdapter1.setAllWords(words);
-                myAdapter2.setAllWords(words);
-                if (temp!=words.size()) {
-                    myAdapter1.notifyDataSetChanged();
-                    myAdapter2.notifyDataSetChanged();
-                }
-
-            }
-        });
-
-        buttonInsert = findViewById(R.id.buttonInsert);
-        buttonClear = findViewById(R.id.buttonClear);
-
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String[] english = {
-                        "Hello",
-                        "World",
-                        "Android",
-                        "Google",
-                        "Studio",
-                        "Project",
-                        "Database",
-                        "Recycler",
-                        "View",
-                        "String",
-                        "Value",
-                        "Integer"
-                };
-                String[] chinese = {
-                        "你好",
-                        "世界",
-                        "安卓系统",
-                        "谷歌公司",
-                        "工作室",
-                        "项目",
-                        "数据库",
-                        "回收站",
-                        "视图",
-                        "字符串",
-                        "价值",
-                        "整数类型"
-                };
-                for(int i = 0;i<english.length;i++) {
-                    wordViewModel.insertWords(new Word(english[i],chinese[i]));
-                }
-            }
-        });
-        buttonClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                wordViewModel.deleteAllWords();
-            }
-        });
-
-
     }
-
-
 }
 
 
---
+++ demos/test-android/Words/app/src/main/java/com/example/words/WordsFragment.java
@@ -0,0 +1,60 @@
+package com.example.words;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class WordsFragment extends Fragment {
+    private WordViewModel wordViewModel;
+    private MyAdapter myAdapter1,myAdapter2;
+    private RecyclerView recyclerView;
+
+
+    public WordsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        wordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+        recyclerView = requireActivity().findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        myAdapter1 = new MyAdapter(false, wordViewModel);
+        myAdapter2 = new MyAdapter(true, wordViewModel);
+
+        wordViewModel.getAllWordsLive().observe(requireActivity(), new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                int temp = myAdapter1.getItemCount();
+                myAdapter1.setAllWords(words);
+                myAdapter2.setAllWords(words);
+                if (temp != words.size()) {
+                    myAdapter1.notifyDataSetChanged();
+                    myAdapter2.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_words, container, false);
+    }
+}
