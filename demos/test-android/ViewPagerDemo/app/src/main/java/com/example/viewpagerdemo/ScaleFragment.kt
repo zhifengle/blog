63ee9babcbb03ed2e0e7c78f5e73bafa25024657
22earth <zhifengle@gmail.com>
2020-12-24 11:12:14 +0800

    update(android): viewpager2 scale

--- demos/test-android/ViewPagerDemo/app/src/main/java/com/example/viewpagerdemo/ScaleFragment.kt
+++ demos/test-android/ViewPagerDemo/app/src/main/java/com/example/viewpagerdemo/ScaleFragment.kt
@@ -1,10 +1,13 @@
 package com.example.viewpagerdemo
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_scale.*
+import kotlin.random.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +20,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ScaleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -37,23 +32,12 @@ class ScaleFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_scale, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScaleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScaleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imageViewScale.setOnClickListener {
+            val factor = if (Random.nextBoolean()) 0.1f else -0.1f
+            ObjectAnimator.ofFloat(it, "ScaleX", it.scaleX + factor).start()
+            ObjectAnimator.ofFloat(it, "ScaleY", it.scaleY + factor).start()
+        }
     }
 }
