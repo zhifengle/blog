8031e529cc779eeca1cf589bd3593c043caa94a5
22earth <zhifengle@gmail.com>
2021-07-19 17:55:54 +0800

    update(rust): nom demo

---
+++ demos/test-rust/hello_cargo/nom-demo/src/main.rs
@@ -0,0 +1,12 @@
+mod arithmetic;
+
+use nom::{bytes::complete::tag, IResult};
+
+fn hello_parser(i: &str) -> IResult<&str, &str> {
+    tag("hello")(i)
+}
+fn main() {
+    println!("{:?}", hello_parser("hello"));
+    println!("{:?}", hello_parser("hello world"));
+    println!("{:?}", hello_parser("goodbye hello again"));
+}
