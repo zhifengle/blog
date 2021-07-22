d32bfbab013168e2350d77020af03655d0278c54
22earth <zhifengle@gmail.com>
2021-07-22 11:05:46 +0800

    update(go): repl

--- demos/test-go/monkey/monkey.go
+++ demos/test-go/monkey/monkey.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"test_go/monkey/repl"
+)
 
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
