94dd70c6ab2101c700e5bed24eeb6f06bcbc5f3e
22earth <zhifengle@gmail.com>
2021-09-04 08:41:05 +0800

    update(rs): link

--- demos/test-rust/hello_cargo/nom-demo/src/main.rs
+++ demos/test-rust/hello_cargo/nom-demo/src/main.rs
@@ -3,6 +3,9 @@ mod sedregex;
 
 use nom::{bytes::complete::tag, IResult};
 
+// https://privaterookie.gitee.io/nom-cheatsheet/
+// https://github.com/fucking-translation/tutorial/blob/main/Rust/nom/%E9%80%89%E6%8B%A9nom%E7%BB%84%E5%90%88%E5%99%A8.md
+
 fn hello_parser(i: &str) -> IResult<&str, &str> {
     tag("hello")(i)
 }
