c88603626ff2e44dc6609e47b2905231b6d84d1d
22earth <zhifengle@gmail.com>
2021-09-09 15:00:19 +0800

    update(ts): edit distance

--- demos/test-ts/src/algorithms/dp.ts
+++ demos/test-ts/src/algorithms/dp.ts
@@ -1,54 +1,75 @@
 export function minDistance(word1: string, word2: string): number {
-  const dp: number[][] = Array.from({ length: word1.length + 1 }, () =>
-    Array.from({ length: word2.length + 1 })
-  )
-  for (let i = 1; i < word2.length + 1; i++) {
-    dp[0][i] = (dp[0][i - 1] || 0) + 1
-  }
-  for (let j = 1; j < word1.length + 1; j++) {
-    dp[j][0] = (dp[j - 1][0] || 0) + 1
-  }
-  for (let i = 1; i < word1.length + 1; i++) {
-    for (let j = 1; j < word2.length + 1; j++) {
+  const len1 = word1.length;
+  const len2 = word2.length;
+  const dp: number[][] = Array.from(Array(len1 + 1), (v, k) => {
+    if (k === 0) {
+      return Array.from(Array(len2 + 1), (v, idx) => idx);
+    }
+    return [k, ...Array(len2).fill(0)];
+  });
+
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
       if (word1[i - 1] === word2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1]
+        dp[i][j] = dp[i - 1][j - 1];
       } else {
         dp[i][j] =
-          (Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) || 0) + 1
+          (Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) || 0) + 1;
       }
     }
   }
-  return dp[word1.length][word2.length]
+  return dp[len1][len2];
+}
+
+// https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C
+export function levenshtein(word1: string, word2: string): number {
+  const len1 = word1.length;
+  const len2 = word2.length;
+  const dp: number[] = Array.from(Array(len1 + 1), (v, k) => k);
+  let old = 0;
+  for (let j = 1; j <= len2; j++) {
+    dp[0] = j;
+    for (let i = 1, last = j - 1; i <= len1; i++) {
+      old = dp[i];
+      dp[i] = Math.min(
+        dp[i] + 1,
+        dp[i - 1] + 1,
+        last + (word1[i - 1] === word2[j - 1] ? 0 : 1)
+      );
+      last = old;
+    }
+  }
+  return dp[len1];
 }
 
 export function rob(nums: number[]): number {
-  const dp = [0, 0]
+  const dp = [0, 0];
   for (let i = 2; i < nums.length + 2; i++) {
-    dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i - 1])
+    dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i - 1]);
   }
-  return dp[nums.length + 1]
+  return dp[nums.length + 1];
 }
 
 export function rob2(nums: number[]): number {
-  let a = 0
-  let b = 0
+  let a = 0;
+  let b = 0;
   for (let i = 0; i < nums.length; i++) {
-    const temp = b
-    b = Math.max(a + nums[i], b)
-    a = temp
+    const temp = b;
+    b = Math.max(a + nums[i], b);
+    a = temp;
   }
-  return b
+  return b;
 }
 
 export function coinChange(coins: number[], amount: number): number {
-  if (amount === 0) return 0
-  const dp: number[] = Array(amount + 1).fill(amount + 1)
-  dp[0] = 0
+  if (amount === 0) return 0;
+  const dp: number[] = Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
   for (let i = 0; i < dp.length; i++) {
     for (let coin of coins) {
-      if (i - coin < 0) continue
-      dp[i] = Math.min(dp[i], 1 + dp[i - coin])
+      if (i - coin < 0) continue;
+      dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
     }
   }
-  return dp[amount] == amount + 1 ? -1 : dp[amount]
+  return dp[amount] == amount + 1 ? -1 : dp[amount];
 }
