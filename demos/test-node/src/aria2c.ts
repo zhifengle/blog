fd5e5fe3e36d9f32b23066e31e8189baba11bb76
22earth <zhifengle@gmail.com>
2021-12-06 17:23:29 +0800

    update(ts): aria2

--- demos/test-node/src/aria2c.ts
+++ demos/test-node/src/aria2c.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import path from 'path';
+import { fetchInfo } from './utils/fetchData';
 
 // https://github.com/sonnyp/aria2.js
 const homedir = require('os').homedir();
@@ -21,3 +22,48 @@ program
     }
   });
 program.parse();
+
+type InfoJson = {
+  name: string;
+  section: string;
+} & any;
+
+async function getAudioUrl(
+  info: InfoJson,
+  entityId: string,
+  entityType: number
+): Promise<string> {
+  const res = await fetchInfo(
+    `http://m.lrts.me/ajax/getPlayPath?entityId=${entityId}&entityType=${entityType}&opType=1&sections=[${info.section}]&type=0`,
+    'json'
+  );
+  console.log(res.msg);
+  return res?.list[0]?.path || '';
+}
+
+async function getBookMenu(entityId: string) {
+  const res = await fetchInfo(
+    `http://m.lrts.me/ajax/getBookMenu?bookId=${entityId}&pageNum=1&pageSize=5000&sortType=0`,
+    'json'
+  );
+  return res?.list;
+}
+
+async function getAlbumAudios(entityId: string) {
+  const res = await fetchInfo(
+    `https://m.lrts.me/ajax/getAlbumAudios?ablumnId=${entityId}&sortType=0`,
+    'json'
+  );
+  return res?.list;
+}
+
+function genAudioExt(url: string): string {
+  if (url.includes('m4a')) {
+    return 'm4a';
+  }
+  return 'mp3';
+}
+
+function getBookList(list: InfoJson[], start: number, end: number) {
+  return list.slice(start, end === 0 ? undefined : end);
+}
