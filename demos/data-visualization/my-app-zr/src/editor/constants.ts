8c82c51eed382bb090cc64fbddc330b4eacbe03c
22earth <zhifengle@gmail.com>
2022-04-18 15:00:07 +0800

    chore: remove

--- demos/data-visualization/my-app-zr/src/editor/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ANCHOR_COLOR = 'rgb(0,181,237)';
-// export const ANCHOR_COLOR = 'red';
-export const BG_COLOR = '#fff';
-export const BOUNDING_RECT_COLOR = '#14f1ff';
--- demos/data-visualization/my-app-zr/src/utils/event-bus.ts
+++
@@ -1,8 +0,0 @@
-import mitt from 'mitt';
-
-export type Events = {
-  'editor/edge': { x1: number; x2: number; y1: number; y2: number };
-};
-
-const emmiter = mitt<Events>();
-export default emmiter;
