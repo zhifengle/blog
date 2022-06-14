248dabc55e0478adaae02b9ade807e368cfebfcf
22earth <zhifengle@gmail.com>
2022-06-14 16:18:34 +0800

    fixed: 7z-arc

--- demos/test-js/node/bin/7z-arc.js
+++ demos/test-js/node/bin/7z-arc.js
@@ -1,17 +1,19 @@
+#!/usr/bin/env node
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const { opendir } = require('fs/promises');
 const path = require('path');
+const fs = require('fs');
 
-const targetDir = String.raw`D:\music`;
+// const targetDir = String.raw`D:\music`;
 const excludes = ['done'];
-// const outputDir = String.raw`D:\d`;
-const outputDir = './';
 
-async function init() {
+async function run(args) {
+  const { targetDir, outputDir, pw } = args;
   try {
-    const dir = await opendir(targetDir);
-    for await (const dirent of dir) {
+    const dirs = await opendir(targetDir);
+    for await (const dirent of dirs) {
       if (
         !new RegExp(excludes.join('|')).test(dirent.name) &&
         dirent.isDirectory()
@@ -19,14 +21,14 @@ async function init() {
         const p = path.join(targetDir, dirent.name);
         const outputPath = path.join(outputDir, dirent.name);
         // 命令存在空格需要使用引号; 可以换成 outputPath; -pXX 密码
-        const s = `7z a "${p}.7z" -mhe -pXX "${p}"`;
+        const s = `7z a "${outputPath}.7z" -mhe -p${pw} "${p}"`;
         console.log('Start: ', p);
         // stdout 乱码参考下面
         const { stderr } = await exec(s);
         if (stderr) {
           console.log(stderr);
         } else {
-          console.log('Ok:', p + '.7z');
+          console.log('Ok:', outputPath + '.7z');
         }
       }
     }
@@ -34,7 +36,44 @@ async function init() {
     console.error(err);
   }
 }
-init();
+function parseArgs(argv) {
+  if (argv[0] !== '-p' || !argv[1]) {
+    console.log('需要指定 password');
+    return;
+  }
+
+  if (!argv[2]) {
+    console.log('需要指定路径');
+    return;
+  }
+  if (!fs.existsSync(argv[2])) {
+    console.log('路径不存在或者不正确');
+    return;
+  }
+  let outputDir = argv[2];
+  if (argv[3] && fs.existsSync(argv[3])) {
+    outputDir = argv[3];
+  }
+  return {
+    pw: argv[1],
+    targetDir: argv[2],
+    outputDir,
+  };
+}
+
+// 压缩目标文件夹里面的文件夹
+// node  7z-arc.js  -p xxx  c:\
+const args = parseArgs(process.argv.slice(2));
+if (!args) {
+  process.exit(1);
+}
+// for test
+// var args = {
+//   pw: 'test',
+//   targetDir: String.raw`C:\tmp\test`,
+//   outputDir: String.raw`C:\tmp\test`,
+// };
+run(args);
 
 // Windows 下乱码
 /*
