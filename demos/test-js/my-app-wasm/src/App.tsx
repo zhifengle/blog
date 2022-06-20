5a4e33ffe9ece96e47719fecefbdce34bf2bcdef
22earth <zhifengle@gmail.com>
2022-06-20 14:29:38 +0800

    chore(js): remove count

--- demos/test-js/my-app-wasm/src/App.tsx
+++ demos/test-js/my-app-wasm/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import init, { add } from 'wasm-lib';
-import { count } from 'console';
 
 function App() {
   const [count, setCount] = useState(0);
