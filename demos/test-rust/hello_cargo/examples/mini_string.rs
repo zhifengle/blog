723dae937164b2d42f5e67fb1671e5523a6bfe6d
22earth <zhifengle@gmail.com>
2022-07-04 15:23:44 +0800

    update(rs): ministring

---
+++ demos/test-rust/hello_cargo/examples/mini_string.rs
@@ -0,0 +1,99 @@
+use std::{fmt, ops::Deref};
+
+// ref: 206  chap15
+const MINI_STRING_MAX_LEN: usize = 30;
+
+struct MiniString {
+    len: u8,
+    data: [u8; MINI_STRING_MAX_LEN],
+}
+impl MiniString {
+    fn new(v: impl AsRef<str>) -> Self {
+        let bytes = v.as_ref().as_bytes();
+        let len = bytes.len();
+        let mut data = [0u8; MINI_STRING_MAX_LEN];
+        data[..len].copy_from_slice(bytes);
+        Self {
+            len: len as u8,
+            data,
+        }
+    }
+}
+
+impl Deref for MiniString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        // data 是来自 str
+        std::str::from_utf8(&self.data[..self.len as usize]).unwrap()
+        // 标准库 String 就是用到这种方式
+        // unsafe { std::str::from_utf8_unchecked(&self.data[..self.len as usize]) }
+    }
+}
+
+impl fmt::Debug for MiniString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.deref())
+    }
+}
+
+#[derive(Debug)]
+enum MyString {
+    Inline(MiniString),
+    Standard(String),
+}
+
+impl Deref for MyString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            MyString::Inline(v) => v.deref(),
+            MyString::Standard(v) => v.deref(),
+        }
+    }
+}
+
+impl From<&str> for MyString {
+    fn from(s: &str) -> Self {
+        match s.len() > MINI_STRING_MAX_LEN {
+            true => Self::Standard(s.to_owned()),
+            _ => Self::Inline(MiniString::new(s)),
+        }
+    }
+}
+
+impl fmt::Display for MyString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.deref())
+    }
+}
+
+fn main() {
+    let len1 = std::mem::size_of::<MyString>();
+    let len2 = std::mem::size_of::<MiniString>();
+    println!("Len: MyString {}, MiniString {}", len1, len2);
+
+    let s1: MyString = "hello world".into();
+    let s2: MyString = "这是一个超过了三十个字节的很长很长的字符串".into();
+
+    // debug 输出
+    println!("s1: {:?}, s2: {:?}", s1, s2);
+    // display 输出
+    println!(
+        "s1: {}({} bytes, {} chars), s2: {}({} bytes, {} chars)",
+        s1,
+        s1.len(),
+        s1.chars().count(),
+        s2,
+        s2.len(),
+        s2.chars().count()
+    );
+
+    // MyString 可以使用一切 &str 接口，感谢 Rust 的自动 Deref
+    assert!(s1.ends_with("world"));
+    assert!(s2.starts_with('这'));
+
+    let s = String::from("这是一个超过了三十个字节的很长很长的字符串");
+    println!("s: {:p}", &*s);
+}
