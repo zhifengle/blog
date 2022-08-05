d13d3a429ebe60f02b644baf5d3fbb7e2a5b8b61
22earth <zhifengle@gmail.com>
2022-08-05 15:58:34 +0800

    update(rs): io demo

--- demos/test-go/go.mod
+++ demos/test-go/go.mod
@@ -3,7 +3,7 @@ module test_go
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
---
+++ demos/test-rust/hello_cargo/examples/io_demo.rs
@@ -0,0 +1,39 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+use std::fs;
+// 对于 file.read_to_string 是必须的
+use std::io::Read;
+use std::{error::Error as StdError, fs::OpenOptions};
+
+// ref: scraping  storage.rs
+
+fn main() {
+    let _s = std::fs::read("Cargo.toml").unwrap();
+}
+
+// serde = { version = "1", features = ["derive"]}
+// serde_json = "1"
+
+// fn get_config(filename: &str) -> Result<serde_json::Value, Box<dyn StdError>> {
+fn get_config(filename: &str) -> Result<(), Box<dyn StdError>> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    if contents.is_empty() {
+        contents = "{}".to_string();
+    }
+    println!("{}", contents);
+    // let contents = fs::read_to_string(filename)?;
+    // let v: serde_json::Value = serde_json::from_str(&contents)?;
+    Ok(())
+}
+
+// 写入文件
+fn write_config(filename: &str, contents: &str) {
+    let _r = fs::write(&filename, contents);
+}
