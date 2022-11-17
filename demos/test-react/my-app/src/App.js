1b4a8509a9459883312bfb28c27e99e523914d37
zhifengle <zhifengle@gmail.com>
2022-11-17 15:52:47 +0800

    update(react): three

--- demos/test-react/my-app/src/App.js
+++ demos/test-react/my-app/src/App.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import Log from './apps/log';
 import SecurityRoute from './apps/security-route/App';
+import ThreeApp from './apps/three';
 
 function App() {
   return <div className="App">app</div>;
 }
 
-export default SecurityRoute;
+export default ThreeApp;
---
+++ demos/test-react/my-app/src/apps/three/index.js
@@ -0,0 +1,14 @@
+import CanvasApp from './fiber-demo';
+
+export default function App() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <CanvasApp />
+    </div>
+  );
+}
