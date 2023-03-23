29b85366c0c57b34522707df9915c312bd5d5316
zhifengle <zhifengle@gmail.com>
2023-03-23 21:41:59 +0800

    update(py): tool

--- demos/py/scraping/tutorial/tools.py
+++ demos/py/scraping/tutorial/tools.py
@@ -3,22 +3,29 @@ import re
 
 
 def get_downloaded_ids(pic_path):
-    download_ids = []
+    download_ids = set()
     for path in Path(pic_path).glob('**/*'):
         if path.is_dir():
             continue
         search_result = re.search(r'(\d+)', path.name)
         if search_result:
             post_id = search_result.group(1)
-            download_ids.append(post_id)
+            download_ids.add(post_id)
     return download_ids
 
 
 def save_downloaded_ids_txt(pic_path):
-    ids = get_downloaded_ids(pic_path)
     cur_path = Path(pic_path)
+    filelist_txt = cur_path / f'{cur_path.name}.txt'
+    
+    downloaded_ids = set()
+    if filelist_txt.exists():
+        with open(filelist_txt, 'r') as f:
+            downloaded_ids.update([line.strip() for line in f.readlines()])
+    
+    downloaded_ids = downloaded_ids.union(get_downloaded_ids(pic_path))
     with open(cur_path / f'{cur_path.name}.txt', 'w') as f:
-        f.write('\n'.join(ids))
+        f.write('\n'.join(list(downloaded_ids)))
 
 
 
